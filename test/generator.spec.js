49cc3acc1975e1374f2e1be8cbcfeb008a11f242
Cyril Casaucau <raiden0610@gmail.com>
2020-07-01 17:35:28 +0200

    Adding test for I18n Fixing test

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -36,23 +36,10 @@ const expectedFiles = {
 };
 
 describe('Flutter JHipster module', () => {
-    describe('Default generation', () => {
+    describe('Default generation without I18n', () => {
         before((done) => {
             helpers
-                .run('generator-jhipster/generators/app')
-                .withOptions({
-                    'from-cli': true,
-                    skipInstall: true,
-                    blueprints: 'flutter',
-                    skipChecks: true
-                })
-                .withGenerators([
-                    [
-                        require('../generators/app'), // eslint-disable-line global-require
-                        'jhipster-flutter:app',
-                        path.join(__dirname, '../generators/app/index.js')
-                    ]
-                ])
+                .run(path.join(__dirname, '../generators/app/index.js'))
                 .withPrompts({
                     baseName: 'sampleDefaultFlutter',
                     packageName: 'com.mycompany.myapp',
@@ -60,20 +47,58 @@ describe('Flutter JHipster module', () => {
                     android: 'kotlin',
                     ios: 'swift',
                     stateManageType: 'bloc',
+                    enableTranslation: false
+                })
+                .on('end', done);
+        });
+
+        it('creates expected files from jhipster flutter generator with localization', () => {
+            assert.file(expectedFiles.common);
+            assert.file(expectedFiles.app);
+            assert.file(expectedFiles.android);
+            assert.noFile(expectedFiles.i18n);
+        });
+
+        it('contains the specific change added by the module with localization', () => {
+            assert.fileContent(`${MAIN_DIR}pubspec.yaml`, 'name: sampleDefaultFlutter');
+            assert.fileContent(`${MAIN_DIR}pubspec.yaml`, 'description: sampleDefaultFlutter flutter project.');
+            assert.fileContent(`${MAIN_DIR}pubspec.yaml`, 'description: sampleDefaultFlutter flutter project.');
+            assert.noFileContent(`${MAIN_DIR}pubspec.yaml`, 'flutter_intl');
+            assert.noFileContent(`${MAIN_DIR}pubspec.yaml`, 'flutter_localizations');
+        });
+    });
+
+    describe('Generation with I18n', () => {
+        before((done) => {
+            helpers
+                .run(path.join(__dirname, '../generators/app/index.js'))
+                .withPrompts({
+                    baseName: 'sampleDefaultFlutter',
+                    packageName: 'com.mycompany.myapp',
+                    directoryPath: path.join(__dirname, '../test/templates/flutter-default/'),
+                    android: 'kotlin',
+                    ios: 'swift',
+                    stateManageType: 'bloc',
+                    enableTranslation: true,
+                    nativeLanguage: 'en',
+                    languages: ['fr']
                 })
                 .on('end', done);
         });
 
         it('creates expected files from jhipster flutter generator', () => {
-            assert.file(expectedFiles.i18n);
             assert.file(expectedFiles.common);
             assert.file(expectedFiles.app);
             assert.file(expectedFiles.android);
+            assert.file(expectedFiles.i18n);
         });
 
-        it('contains the specific change added by the blueprint', () => {
+        it('contains the specific change added by the module', () => {
             assert.fileContent(`${MAIN_DIR}pubspec.yaml`, 'name: sampleDefaultFlutter');
             assert.fileContent(`${MAIN_DIR}pubspec.yaml`, 'description: sampleDefaultFlutter flutter project.');
+            assert.fileContent(`${MAIN_DIR}pubspec.yaml`, 'description: sampleDefaultFlutter flutter project.');
+            assert.fileContent(`${MAIN_DIR}pubspec.yaml`, 'flutter_intl');
+            assert.fileContent(`${MAIN_DIR}pubspec.yaml`, 'flutter_localizations');
         });
     });
 });
