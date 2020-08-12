9d52fcfc72cf92e1d12af8e886f4d6fa152457d1
Cyril Casaucau <raiden0610@gmail.com>
2020-08-12 10:47:32 +0200

    Complete unit tests

--- test/entity.spec.js
+++ test/entity.spec.js
@@ -10,20 +10,26 @@ const expectedFiles = {
     employeeBloc: [
         `${EMPLOYEE_ENTITIES_MAIN_DIR}/bloc/employee_bloc.dart`,
         `${EMPLOYEE_ENTITIES_MAIN_DIR}/bloc/employee_events.dart`,
-        `${EMPLOYEE_ENTITIES_MAIN_DIR}/bloc/employee_state.dart`
+        `${EMPLOYEE_ENTITIES_MAIN_DIR}/bloc/employee_state.dart`,
+        `${EMPLOYEE_ENTITIES_MAIN_DIR}/bloc/employee_form_model.dart`
     ],
     employeeMain: [
         `${EMPLOYEE_ENTITIES_MAIN_DIR}/employee_list_screen.dart`,
+        `${EMPLOYEE_ENTITIES_MAIN_DIR}/employee_update_screen.dart`,
+        `${EMPLOYEE_ENTITIES_MAIN_DIR}/employee_view_screen.dart`,
         `${EMPLOYEE_ENTITIES_MAIN_DIR}/employee_model.dart`,
         `${EMPLOYEE_ENTITIES_MAIN_DIR}/employee_repository.dart`
     ],
     jobBloc: [
         `${JOB_ENTITIES_MAIN_DIR}/bloc/job_bloc.dart`,
         `${JOB_ENTITIES_MAIN_DIR}/bloc/job_events.dart`,
+        `${JOB_ENTITIES_MAIN_DIR}/bloc/job_form_model.dart`,
         `${JOB_ENTITIES_MAIN_DIR}/bloc/job_state.dart`
     ],
     jobMain: [
         `${JOB_ENTITIES_MAIN_DIR}/job_list_screen.dart`,
+        `${JOB_ENTITIES_MAIN_DIR}/job_update_screen.dart`,
+        `${JOB_ENTITIES_MAIN_DIR}/job_view_screen.dart`,
         `${JOB_ENTITIES_MAIN_DIR}/job_model.dart`,
         `${JOB_ENTITIES_MAIN_DIR}/job_repository.dart`
     ]
